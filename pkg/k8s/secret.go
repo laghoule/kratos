50d99c227186d9cc674075a7b3a6d6bc8a9693db
Pascal Gauthier <pgauthier@nihilisme.ca>
2021-12-18 15:58:27 -0500

    Add checkSecretOwnership For protection, we check if the secret is owned my kratos.
    Signed-off-by: Pascal Gauthier <pgauthier@nihilisme.ca>

--- pkg/k8s/secret.go
+++ pkg/k8s/secret.go
@@ -9,12 +9,32 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// TODO add isSecretSafeToUse for protection
-// TODO refactor CreateUpdateSecret to work as the other CreateUpdateObject
-// TODO add a CreateUpdateConfiguration, for storing kratos release configuration
+// TODO: refactor CreateUpdateSecret to work as the other CreateUpdateObject
+// TODO: add a CreateUpdateConfiguration, for storing kratos release configuration
+
+// checkSecretOwnership check if it's safe to create, update or delete the secret
+func (c *Client) checkSecretOwnership(name, namespace string) error {
+	svc, err := c.Clientset.CoreV1().Secrets(namespace).Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return nil
+		}
+		return fmt.Errorf("getting secret failed: %s", err)
+	}
+
+	if svc.Labels[depLabelName] == name {
+		return nil
+	}
+
+	return fmt.Errorf("secret is not owned by kratos")
+}
 
 // CreateUpdateSecret a secret to namespace
 func (c *Client) CreateUpdateSecret(secret *corev1.Secret, namespace string) error {
+	if err := c.checkSecretOwnership(secret.Name, namespace); err != nil {
+		return err
+	}
+
 	_, err := c.Clientset.CoreV1().Secrets(namespace).Create(context.Background(), secret, metav1.CreateOptions{})
 	if err != nil {
 		if errors.IsAlreadyExists(err) {
@@ -32,6 +52,10 @@ func (c *Client) CreateUpdateSecret(secret *corev1.Secret, namespace string) err
 
 // DeleteSecret delete a secret from a namespace
 func (c *Client) DeleteSecret(name, namespace string) error {
+	if err := c.checkSecretOwnership(name, namespace); err != nil {
+		return err
+	}
+
 	if err := c.Clientset.CoreV1().Secrets(namespace).Delete(context.Background(), name, metav1.DeleteOptions{}); err != nil {
 		return fmt.Errorf("deleting secret %s failed: %s", name, err)
 	}
