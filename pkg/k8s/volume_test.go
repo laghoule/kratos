b2341facdbcaf5685590c3eaa79410ee7ba1808d
Pascal Gauthier <pgauthier@nihilisme.ca>
2022-01-07 20:36:55 -0500

    Add TestGetVolumesConfForContainer
    Signed-off-by: Pascal Gauthier <pgauthier@nihilisme.ca>

--- pkg/k8s/volume_test.go
+++ pkg/k8s/volume_test.go
@@ -2,8 +2,20 @@ package k8s
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetVolumesConfForContainer(t *testing.T) {
-	// TODO: TestGetVolumesConfForContainer
+	d, err := newDeployment()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	dep := createDeployment()
+
+	volMounts, vols := getVolumesConfForContainer(name, &d.Deployment.Containers[0], d.Config)
+	assert.Equal(t, dep.Spec.Template.Spec.Containers[0].VolumeMounts, volMounts)
+	assert.Equal(t, dep.Spec.Template.Spec.Volumes, vols)
 }
